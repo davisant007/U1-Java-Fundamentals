0e966d1c58a80fecc712cf8abb746f0a0cb97aa4
davisant007 <davisant007@warren.k12.in.us>
2021-08-04 12:52:39 -0400

    completed L1 and L2 notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,21 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+
 public class L2 {
 
     //add the main method header
-}
+    public static void main(String[] args) {
+        double num1 = 3.4;
+        boolean num2 = false;
+        String num3 = "4.5";
+
+        String firstName = "George";
+        int age = 16;
+        System.out.println(firstName);
+        System.out.println(age);
+
+
+    }
+ String name ="anthony";
+    int accountNumber = 123456789
+            double balance = 123456789
+                     System.out.println(firstName)
